1f828ceb0006ef7d93f8923e4a70c97d4acd741e
Andrey Ramos <aramos@cruxconsultores.com>
2023-05-26 14:40:09 -0600

    Reporte financiero con mejora en calculos

--- CherryPick/ReporteFinancieroBus/ReporteFinanciero.cs
+++ CherryPick/ReporteFinancieroBus/ReporteFinanciero.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace ReporteFinancieroBus
 {
-    internal class ReporteFinanciero
+    public class ReporteFinanciero
     {
+        public void HacerCalculos() { }
     }
 }
